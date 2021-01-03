b417a129ed99a78d0bff88ae099bbc494682677a
FENR1 USER <fenr1user@biadsconsulting.org>
2021-01-03 20:31:52 +0600

    first commit

---
+++ january/js_practice/script.js
@@ -0,0 +1,8 @@
+// ............. ES12 .... ECMAScript  2021................. release in the month of June 2021
+let x = 1;
+let y = 2;
+x &&=y;
+console.log(x) // output will be 2
+
+let txt = "Hello World, Hello Earth";
+console.log(txt.replaceAll("Hello", "Covid")) // Covid World, Covid Earth
