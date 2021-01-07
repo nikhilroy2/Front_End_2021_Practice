50178ab150f6f7675333da00b449d202d4e44abe
FENR1 USER <fenr1user@biadsconsulting.org>
2021-01-07 12:55:45 +0600

    janu7.js added

---
+++ january/js_practice/janu7.js
@@ -0,0 +1,18 @@
+let arr = ["Nikhil", "Sonchoy", "Boku"]
+let fill = arr.filter((a,b,c)=> {
+    return a===arr[0]
+})
+console.log(fill)
+
+// need to learn array iteration
+// forEach()
+// map()
+// filter()
+// reduce()
+// reduceRight()
+// every()
+// some()
+// indexOf()
+// lastIndexOf()
+// find()
+// findIndex()
